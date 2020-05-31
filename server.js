35fb43178b463d48ea9b08df35fc517a59e6a7dc
Andy Kawabata <andykawabata@gmail.com>
2020-05-31 19:53:06 -0400

    added ejs

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const fetch = require("node-fetch");
+app.set('view engine', 'ejs');
+app.listen(process.env.PORT || 8080, ()=>console.log("listening..."))
 
-app.use(express.static('public'))
+let enviornmentVariable = process.env.VARNAME
+let todo; 
 
-app.listen(process.env.PORT || 8080, ()=>console.log("Yes"))
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => todo = json)
+
+
+
+
+app.get('/', function(req, res){
+    res.render('home', {todo: todo})
+});
+app.get('/:id', function(req, res){
+    res.render('home', {id: req.params.id})
+});
